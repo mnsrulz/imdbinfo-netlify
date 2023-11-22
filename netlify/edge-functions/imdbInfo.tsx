621775ed69b738628bac5deea69691af8b504be9
mnsrulz <manishrawat4u@gmail.com>
2023-11-22 03:05:47 +0000

    better error message if the request doesn't provide imdbid

--- netlify/edge-functions/imdbInfo.tsx
+++ netlify/edge-functions/imdbInfo.tsx
@@ -2,14 +2,15 @@ import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 
 export default async function handler(req: Request) {
     const imdbId = new URL(req.url).pathname.split('/').pop();
+    if (!imdbId) return Response.json({ error: "Bad Request! Must provide imdbId in the path e.g. https://imdbinfoapi.netlify.app/tt20913276" }, { status: 400 });
     const resp = await fetch(`https://www.imdb.com/title/${imdbId}`);
-    
+
     if (resp.ok) {
         const htmlContent = await resp.text();
         const document = new DOMParser().parseFromString(htmlContent, "text/html");
         const scriptJsonElement = document.querySelector('script[type="application/ld+json"]')
         const jsonResponse = JSON.parse(scriptJsonElement.innerHTML);
-        const duration =jsonResponse.duration?.replace(/PT(\d+)H(\d+)M/, "$1:$2") || '';
+        const duration = jsonResponse.duration?.replace(/PT(\d+)H(\d+)M/, "$1:$2") || '';
         const dataToReturn = {
             imdbId,
             rating: `${jsonResponse.aggregateRating?.ratingValue}`,
@@ -18,12 +19,12 @@ export default async function handler(req: Request) {
         }
         return Response.json(dataToReturn);
     } else {
-        return  Response.json({error:"Bad Request"}, {
+        return Response.json({ error: "Bad Request" }, {
             status: 400
         });
     }
 }
 
 export const config = {
-  path: "/*",
+    path: "/*",
 };
